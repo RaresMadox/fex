b24b87cddee957e9fe13e915fa758ac27b562dc0
Rares <rares.petre897@gmail.com>
2026-05-08 13:49:35 +0300

    add window widget

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <QApplication>
 #include <QLabel>
 #include <QWidget>
+#include <QVBoxLayout>
 
 //----------------------------------------------------------------
 int main(int argc,char *argv[]){
+
     QApplication app(argc,argv);
 
-    QLabel label("Hello World!");
+    QWidget *window = new QWidget();
+    QVBoxLayout *layout = new QVBoxLayout(window);
+    QLabel *label = new QLabel("Hello World!");
+    label->setAlignment(Qt::AlignCenter);
 
-    label.resize(400,200);
 
-    label.show();
-
+    label->resize(400,200);
+    window->setFixedSize(650,400);
+    
+    layout->addWidget(label,Qt::AlignCenter);
+    
+    window->show();
     return app.exec();
 }
