60e34f5a28db3121342bec5b112e47319c40a9ec
Rares <rares.petre897@gmail.com>
2026-05-08 16:34:17 +0300

    Create CustomApp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,11 @@
-#include <QApplication>
-#include "MainWindow.h"
-#include <QFile>
-#include <QTextStream>
-
+#include "CustomApp.h"
 
 //----------------------------------------------------------------
 int main(int argc,char *argv[]){
 
-    QApplication app(argc,argv);
+    CustomApp app(argc,argv);
     
-    QFile file(":/style.qss"); 
     
-    if (!file.exists()) {
-        qDebug() << "EROARE: Fisierul nu exista la calea specificata!";
-    }
-
-    if (file.open(QFile::ReadOnly | QFile::Text)) {
-        QTextStream stream(&file);
-        QString styleSheet = stream.readAll();
-        
-        
-        app.setStyleSheet(styleSheet);
-        
-        file.close();
-    }
 
     MainWindow window;
     
